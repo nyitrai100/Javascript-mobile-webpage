c4bf069f194ca5b8893efe0a2b95b6ff58c143fa
nyitrai100 <nyitrai100@gmail.com>
2022-12-03 08:40:29 +0000

    js

---
+++ mobile.js
@@ -0,0 +1,31 @@
+const phones = {
+    0: {
+     Brand: "Apple",
+     Name: "Iphone 12pro",
+     Memory: "512GB",
+     Cost: 1149,
+     Colors: ["Gold", "Silver", "Gray",],
+     Ext: ".png",
+    },
+
+    1: {
+        Brand: "Apple",
+        Name: "Iphone SE 2020",
+        Memory: "64GB",
+        Cost: 349,
+        Colors: ["Red", "White",],
+        Ext: ".png",
+        },
+
+    2: {
+        Brand: "Apple",
+        Name: "Iphone 13 mini",
+        Memory: "256GB",
+        Cost: 729,
+        Colors: ["Blue", "White", "Pink", "Black", ],
+        Ext: ".png",
+        },
+    
+
+    
+}
