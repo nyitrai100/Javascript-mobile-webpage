8aa4d5cb99fb1078f4f8e09e602d7762eb5294b8
nyitrai100 <nyitrai100@gmail.com>
2022-12-05 16:25:41 +0000

    js

---
+++ default.js
@@ -0,0 +1,41 @@
+function submit(){
+    const menu = document.querySelector("#Menu");
+    // Brands
+    const brands = menu.querySelectorAll("div[name='brand']");
+    const brandCheckboxes = [];
+    brands.forEach(function(brand){
+        const checkbox = brand.querySelector("input[type='checkbox']");
+        if(checkbox.checked === true)
+            brandCheckboxes.push(checkbox.id);
+    });
+    // Memory
+    const memories = menu.querySelectorAll("div[name='memory']");
+    const memoryCheckboxes = [];
+    memories.forEach(function(memory){
+        const checkbox = memory.querySelector("input[type='checkbox']");
+        if(checkbox.checked === true)
+        memoryCheckboxes.push(checkbox.id);
+    })
+    // Color
+    const colors = menu.querySelectorAll("div[name='color']");
+    const colorCheckboxes = [];
+    colors.forEach(function(color){
+        const checkbox = color.querySelector("input[type='checkbox']");
+        if(checkbox.checked === true)
+        colorCheckboxes.push(checkbox.id)
+        
+    })
+    // ONLY ONCE
+    const mobiles = [];
+    phones.forEach(function(phone){
+        if (brandCheckboxes.includes(phone.Brand) && memoryCheckboxes.includes(phone.Memory) && colorCheckboxes.includes(phone.Color)) {
+            mobiles.push(phone);
+        }
+        
+       
+    });
+
+    console.log(mobiles);
+}
+
+
--- mobile.js
+++ mobile.js
@@ -1,41 +1,110 @@
-const phones = {
-    0: {
+const phones = [
+  
+    {
      Brand: "Apple",
      Name: "Iphone 12pro",
      Memory: "512GB",
      Cost: 1149,
-     Colors: ["Gold", "Silver", "Gray",],
+     Color: "Gold",
      Ext: ".png",
     },
+    {
+        Brand: "Apple",
+        Name: "Iphone 12pro",
+        Memory: "512GB",
+        Cost: 1149,
+        Color: "Silver",
+        Ext: ".png",
+       },
 
-    1: {
+    {
+        Brand: "Apple",
+        Name: "Iphone 12pro",
+        Memory: "512GB",
+        Cost: 1149,
+        Color: "Gray",
+        Ext: ".png",
+       },
+
+    {
         Brand: "Apple",
         Name: "Iphone SE 2020",
         Memory: "64GB",
         Cost: 349,
-        Colors: ["Red", "White",],
+        Color: "Red",
         Ext: ".png",
         },
 
-    2: {
+        {
+        Brand: "Apple",
+        Name: "Iphone SE 2020",
+        Memory: "64GB",
+        Cost: 349,
+        Color: "White",
+        Ext: ".png",
+        },
+         {
         Brand: "Apple",
         Name: "Iphone 13 mini",
         Memory: "256GB",
         Cost: 729,
-        Colors: ["Blue", "White", "Pink", "Black", ],
+        Color: "Blue",
         Ext: ".png",
         },
+        {
+        Brand: "Apple",
+        Name: "Iphone 13 mini",
+        Memory: "256GB",
+        Cost: 729,
+        Color: "White",
+        Ext: ".png",
+        },
+        {
+        Brand: "Apple",
+        Name: "Iphone 13 mini",
+        Memory: "256GB",
+        Cost: 729,
+        Color: "Pink",
+        Ext: ".png",
+         },
+        {
+        Brand: "Apple",
+        Name: "Iphone 13 mini",
+        Memory: "256GB",
+        Cost: 729,
+        Color: "Black",
+        Ext: ".png",
+                    },
 
-    3: {
+    {
         Brand: "Google",
         Name: "Pixel 6A",
         Memory: "128GB",
         Cost: 399,
-        Colors: ["Green", "White", "Black",],
+        Color: "Green",
         Ext: ".png",
         },
 
-    4: {
+        {
+        Brand: "Google",
+        Name: "Pixel 6A",
+        Memory: "128GB",
+        Cost: 399,
+        Color: "White",
+        Ext: ".png",
+        },
+             
+        {
+        Brand: "Google",
+        Name: "Pixel 6A",
+        Memory: "128GB",
+        Cost: 399,
+        Color :"Black",
+        Ext: ".png",
+        },
+        
+
+    {
         Brand: "Google",
         Name: "Pixel 6Pro",
         Memory: "256GB",
@@ -44,7 +113,7 @@ const phones = {
         Ext: ".png",
         },
         
-    5: {
+    {
         Brand: "Motorola",
         Name: "G82",
         Memory: "128GB",
@@ -53,41 +122,100 @@ const phones = {
         Ext: ".png",
         },
     
-    6: {
+    {
         Brand: "Motorola",
         Name: "Moto E30",
         Memory: "32GB",
         Cost: 89,
-        Colors: ["Blue", "Gray",],
+        Color: "Gray",
         Ext: ".png",
         },
+        {
+        Brand: "Motorola",
+        Name: "Moto E30",
+        Memory: "32GB",
+        Cost: 89,
+        Color: "Blue",
+        Ext: ".png",
+            },
 
-     7: {
+     {
         Brand: "Samsung",
         Name: "Galaxy S22 Plus",
         Memory: "128GB",
         Cost: 949,
-        Colors: ["Green", "Black", "White", "Pink",],
+        Color: "Green",
         Ext: ".png",
         },
 
-    8: {
+     {
+        Brand: "Samsung",
+        Name: "Galaxy S22 Plus",
+        Memory: "128GB",
+        Cost: 949,
+        Color: "Black",
+        Ext: ".png",
+        },
+
+         {
+        Brand: "Samsung",
+        Name: "Galaxy S22 Plus",
+        Memory: "128GB",
+        Cost: 949,
+        Color: "White",
+        Ext: ".png",
+        },
+         {
+        Brand: "Samsung",
+        Name: "Galaxy S22 Plus",
+        Memory: "128GB",
+        Cost: 949,
+        Color: "Pink",
+        Ext: ".png",
+        },
+
+    {
         Brand: "Samsung",
         Name: "Galaxy A13",
         Memory: "64GB",
         Cost: 129,
-        Colors: ["Black", "White",],
+        Color: "Black",
+        Ext: ".png",
+        },
+     {
+        Brand: "Samsung",
+        Name: "Galaxy A13",
+        Memory: "64GB",
+        Cost: 129,
+        Color: "White",
         Ext: ".png",
         },
             
-    9: {
+    {
         Brand: "Nokia",
         Name: "105",
         Memory: "4MB",
         Cost: 25,
-        Colors: ["Blue", "Black", "Pink",],
+        Color: "Blue",
+        Ext: ".png",
+        },
+    
+     {
+        Brand: "Nokia",
+        Name: "105",
+        Memory: "4MB",
+        Cost: 25,
+        Color: "Black",
+        Ext: ".png",
+        },
+     {
+        Brand: "Nokia",
+        Name: "105",
+        Memory: "4MB",
+        Cost: 25,
+        Color: "Pink",
         Ext: ".png",
         },
 
     
-}
+    ]
